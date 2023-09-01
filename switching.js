fb87dd6d067b788b6b3f4a170a7b647134e1cd4e
Debasish Bhola <devop69@gmail.com>
2023-09-01 11:11:53 +0530

    Create switching.js

---
+++ switching.js
@@ -0,0 +1,12 @@
+var a = 9;
+var b = 2;
+
+console.log("Value of a is: "+a);
+console.log("Value of a is: "+b);
+
+var c = a;
+a = b;
+b = c;
+
+console.log("New value of a is: "+a);
+console.log("New value of b is: "+b)
