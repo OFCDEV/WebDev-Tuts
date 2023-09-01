5bac5c45235e43672ee0d5e33fc1f4f9dbcf70ba
Debasish Bhola <devop69@gmail.com>
2023-09-01 11:11:14 +0530

    Create Index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+var myName = "Dev";
+var yourName = prompt("Enter your name: ");
+alert("My name is "+myName + " How's your day " +yourName+ "?")
+//you can use "typeof" to know what kind of variable you're using
