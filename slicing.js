228ec5c09d1cdc43b4699c880c17c983718a51b3
Debasish Bhola <devop69@gmail.com>
2023-09-03 20:40:03 +0530

    Update

--- slicing.js
+++ slicing.js
@@ -2,4 +2,4 @@ var name = "Debasish";
 
 name.slice = (0,1);
 //In slice the 1st quater represents the position of the word which u want to remove.
-//and the 2nd quater represents the max range.s
+//and the 2nd quater represents the max range.
