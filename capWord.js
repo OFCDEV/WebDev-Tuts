c32f592a5b6cfd6b3f42482e2906659f4597ef26
Debasish Bhola <devop69@gmail.com>
2023-09-04 21:42:16 +0530

    Update capWord.js

--- capWord.js
+++ capWord.js
@@ -2,4 +2,8 @@ var name = prompt("Enter your name: ");
 
 var letter1 = name.slice(0,1);
 
+var capLetter = letter1.toUpperCase();
+
 var restLetter = name.slice(1,name.length);
+
+var capName = letter1 + restLetter ;
