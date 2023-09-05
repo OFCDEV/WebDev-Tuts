0a55298f9ea5cb13d04aa8382eff1ad6bd41d3f4
Debasish Bhola <devop69@gmail.com>
2023-09-05 20:21:38 +0530

    update

--- moveBeeper.js
+++ moveBeeper.js
@@ -1,2 +1,17 @@
 //https://stanford.edu/~cpiech/karel/ide.html --Link to the site
 
+function main(){
+    putBeeper();
+    diagonalMove();
+    diagonalMove();
+    diagonalMove();
+    diagonalMove();
+ }
+ 
+ function diagonalMove() {
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+    }
