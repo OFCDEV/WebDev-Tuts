60bce71cc12f30343338263e5404b7e31d08247e
Debasish Bhola <devop69@gmail.com>
2023-09-06 11:21:34 +0530

    Update

--- getMilk.js
+++ getMilk.js
@@ -11,6 +11,8 @@ function getMilk(money) {
     console.log("moveRight");
 
     var numOfBottles = Math.floor(money / 1.5);
+    console.log("Buy " +numOfBottles + " number of bottles of Milk.")
+
     console.log("moveLeft");
     console.log("moveLeft");
     console.log("moveDown");
