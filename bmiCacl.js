9d67811e0ef56e240896f93ee890f17aa37a0ca4
Debasish Bhola <devop69@gmail.com>
2023-09-08 20:50:57 +0530

    Create loveCalc.js

--- bmiCacl.js
+++ bmiCacl.js
@@ -1,6 +1,7 @@
 function bmiCalculator(weight , height) {
     var bmi = weight / Math.pow(height , 2);
-    return bmi;
+    return Math.round(bmi); 
+    //It gives us a round figure similar to Math.floor
 }
 
 console.log(bmiCalculator(50 , 1.67));
