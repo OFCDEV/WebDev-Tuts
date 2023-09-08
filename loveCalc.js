878153a43f3cfe59f7ce892c9770ecb72cc188a4
Debasish Bhola <devop69@gmail.com>
2023-09-08 20:54:03 +0530

    Update loveCalc.js

---
+++ loveCalc.js
@@ -0,0 +1,7 @@
+prompt("Enter your name: ");
+prompt("Enter your loved one's name: ");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1 ;
+
+alert("Your love percentage is: " +loveScore+ "%");
