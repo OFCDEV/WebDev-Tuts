21c64ebc6f571e9264cea2e32c865abe33f3b547
Debasish Bhola <devop69@gmail.com>
2023-09-09 19:17:47 +0530

    Update guestList.js

--- guestCheck.js
+++ guestCheck.js
@@ -3,5 +3,7 @@ var guestList = ["Sambhav","Dev" , "Shreyo" , "Anandita" , "Roshu" ];
 var guestName = prompt("Enter your guest ID: ");
 
 if(guestList.includes(guestName)) {
-    alert
+    alert("You're welcome.")
+}else{
+    alert("Sorry! Maybe next time.")
 }
