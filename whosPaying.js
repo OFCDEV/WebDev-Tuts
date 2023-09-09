51dbed7037c7e47b2dbd97de9eeb47d063233dc5
Debasish Bhola <devop69@gmail.com>
2023-09-10 01:21:55 +0530

    Update whosPayong,js

---
+++ whosPaying.js
@@ -0,0 +1,6 @@
+function whosPaying (names){
+    var numberOfPepople = names.length;
+    var randomPosition = Math.floor(Math.random() * numberOfPepople);
+    var randomPepole = names.[randomPosition];
+
+}
