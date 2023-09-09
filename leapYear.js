a7387bc8aa795f18be077ac9ef8f71d7a29117bd
Debasish Bhola <devop69@gmail.com>
2023-09-09 18:55:25 +0530

    Update leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -1,7 +1,11 @@
 function isLeap(year) {
     if(year % 4 === 0){
         if(year % 100 === 0){
-
+            if(year % 400 === 0){
+                return "Leap Year."
+            }else{
+                return "Not Leap Year."
+            }
         }else{
             return "Leap Year."
         }
