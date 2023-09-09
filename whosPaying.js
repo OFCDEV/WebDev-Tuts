f1040e695d8fe9891fc2e6cfaf90e0c56cb901e1
Debasish Bhola <devop69@gmail.com>
2023-09-10 01:22:26 +0530

    Udate

--- whosPaying.js
+++ whosPaying.js
@@ -2,5 +2,5 @@ function whosPaying (names){
     var numberOfPepople = names.length;
     var randomPosition = Math.floor(Math.random() * numberOfPepople);
     var randomPepole = names.[randomPosition];
-
+    return randomPepole + " is going to pay."
 }
