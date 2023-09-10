c8b2bab01001df38eab788e3d115025449e4165e
Debasish Bhola <devop69@gmail.com>
2023-09-10 19:42:51 +0530

    Update fbSeries.js

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -1,4 +1,4 @@
-function fibonacciSeries(n) {
+function fibonacciGenarator(n) {
     var output = [];
 
     if(n === 1){
@@ -8,12 +8,13 @@ function fibonacciSeries(n) {
         output = [0,1];
     }
     else{
-        for(var i =0; i<n ; i++){
-            output = output.push(output[output.length-2] + output[output.length-1]);
+        output[0,1];
+        for(var i =2; i<n ; i++){
+            output.push(output[output.length-2] + output[output.length-1]);
         }
     }
     return output;
 }
 
-output = fibonacciSeries(5);
+output = fibonacciGenarator(5);
 console.log(output);
