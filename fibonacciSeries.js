2318758c015a249a97c56d55cc158656b8608a7e
Debasish Bhola <devop69@gmail.com>
2023-09-10 19:06:05 +0530

    Update fibonacciSeries.js

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -7,4 +7,10 @@ function fibonacciSeries(n) {
     else if(n === 2){
         output = [0,1];
     }
-   
+    else{
+        for(var i =0; i<n ; i++){
+            output = output.push(output[output.length-2] + output[output.length-1]);
+        }
+    }
+    return output;
+}
