95a67e6e0cff705f203375d9d2d6a6db11a38ad9
Debasish Bhola <devop69@gmail.com>
2023-12-19 23:00:53 +0530

    Bug Fix

--- whosPaying.js
+++ whosPaying.js
@@ -1,6 +1,6 @@
 function whosPaying (names){
     var numberOfPepople = names.length;
     var randomPosition = Math.floor(Math.random() * numberOfPepople);
-    var randomPepole = names.[randomPosition];
+    var randomPepole = names[randomPosition];
     return randomPepole + " is going to pay."
 }
