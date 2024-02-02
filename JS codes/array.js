e07f6d3426ed6267f546cb9df82732d1a2f437c8
Debasish Bhola <devop69@gmail.com>
2024-02-02 14:25:43 +0530

    Arrays func

---
+++ JS codes/array.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5,6];
+console.log(arr);   //Returns the array
+console.log(arr.length);   //Returns the length of the array
+console.log(arr.toString());   //Returns a string separated by comma
+console.log(arr.join("and"));   //Returns the string separated by and
+console.log(arr.pop());   //Returns the array and removes the last element
+console.log(arr.push(599));   //Returns the array and add 599 at the end
+console.log(arr.shift());   //Returns the array and remove the first element
+console.log(arr.unshift("Hiee"));   //Returns the arrray and insert element in the first position
