05c3232c8e421dc0600b365ffcf78f1228b25512
Debasish Bhola <devop69@gmail.com>
2024-02-03 19:08:10 +0530

    DOM-2

---
+++ DOM/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+
+//let cont = doucment.body.childNodes -->Shows all the child elements including whitespaces as text
+
+//document.body.childNodes[1]--> Accessing the index 1 element
+
+//document.body.firstElementChild --> Shows first child element not including whitespace
+
+//document.body.firstElementChild.children --> Gives the array of all elements in first element
+
+// document.body.firstElementChild.nextElementSibling-->Gives next element of the container
+
+// document.body.firstElementChild.previousElementSibling-->Gives prev element of the container
