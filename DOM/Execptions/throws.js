902e44a4680bb62fa47f5ad27247dc9a73360750
Debasish Bhola <devop69@gmail.com>
2024-02-09 09:17:40 +0530

    throws.js Completed

--- DOM/Execptions/throws.js
+++ DOM/Execptions/throws.js
@@ -3,6 +3,11 @@ let a = prompt("Enter the first number:")
 let b = prompt("Enter the second number:")
 
 //promt passes a string; so if we want mathematical calculation we have to do typecasting
+//If user by chance passed some string we have to give an error
+if (isNaN(a) || isNaN(b)) {
+    throw SyntaxError("Sorry This is not allowed.")
+}
+
 let sum = parseInt(a) + parseInt(b);
 
 console.log("The sum is", sum);
