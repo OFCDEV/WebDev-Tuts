18117f741cedb613d60f6528794dba00a2ca0efd
Debasish Bhola <devop69@gmail.com>
2024-02-09 09:13:21 +0530

    Exception Handling

---
+++ DOM/Execptions/throws.js
@@ -0,0 +1,9 @@
+let a = prompt("Enter the first number:")
+
+let b = prompt("Enter the second number:")
+
+//promt passes a string; so if we want mathematical calculation we have to do typecasting
+let sum = parseInt(a) + parseInt(b);
+
+console.log("The sum is", sum);
+
