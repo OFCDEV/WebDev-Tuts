6cda0390d09a7c314c53372faa28b609508282cb
Debasish Bhola <devop69@gmail.com>
2024-02-19 20:49:34 +0530

    Mongoose Completed

--- MongoDB/moongose.js
+++ MongoDB/moongose.js
@@ -2,5 +2,20 @@
 
 
 import mongoose from "mongoose";
+import express from "express";
+import { Todo } from "./models/Todo.js";
 
-await mongoose.connect("mongodb://localhost:27017/")
+let connection = await mongoose.connect("mongodb://localhost:27017/todo")
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    const todo = new Todo({title:"My first todo",desc:"Description of this code",isDone:false})
+    todo.save();
+    res.send('Hello World');
+})
+
+app.listen(port,()=>{
+    console.log(`App is running on ${3000}`)
+})
