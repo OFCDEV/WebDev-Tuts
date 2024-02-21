2590a0121b4a22d309da84c0a788c77db6c0a421
Debasish Bhola <devop69@gmail.com>
2024-02-21 09:33:42 +0530

    Compoments in React

---
+++ ReactFiles/second-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../components/Footer.css "
+const Footer = () => {
+  return (
+    <div>
+      copyright @devV || All rights reserved
+    </div>
+  )
+}
+
+export default Footer
---
+++ ReactFiles/second-app/src/components/Navbar2.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../components/Navbar.css"
+
+function Navbar2() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar2;
