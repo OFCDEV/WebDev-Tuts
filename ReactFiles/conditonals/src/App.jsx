c514cf64b076a6472128e2caacac4642d233112d
Debasish Bhola <devop69@gmail.com>
2024-02-23 23:40:21 +0530

    Completed

--- ReactFiles/conditonals/src/App.jsx
+++ ReactFiles/conditonals/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showbtn, setShowbtn] = useState(false)
 
   return (
     <>
@@ -17,9 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      {/* If-else method */}
+      {/* {showbtn ? <button>Visible hogya</button> : <button>Visible nhi hua</button>} */}
+    {showbtn && <button>Visible hogya</button>}
+    {/* React method */}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setShowbtn(!showbtn)}>
+          Magic Dekh!
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
