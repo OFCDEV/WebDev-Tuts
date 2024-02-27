38a36bafd8bbfc103a41ea47cbb032cbce65bd3d
Debasish Bhola <devop69@gmail.com>
2024-02-28 00:42:52 +0530

    fetchAPI data

--- ReactFiles/ExerciseQ1/src/App.jsx
+++ ReactFiles/ExerciseQ1/src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setcards] = useState([]);
   //https://jsonplaceholder.typicode.com/posts
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let data = await a.json();
+    setcards(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <>
-      
-    </>
+  <>
+    <div className="cointainer">
+      {cards.map((card)=>{
+        return <div key={card.id} className="card">
+          <h1>{card.title}</h1>
+          <p>{card.body}</p>
+          <span>By: UserId: {card.userId}</span>
+        </div>
+      })}
+    </div>
+  </>
   )
 }
 
-export default App
+export default App;
